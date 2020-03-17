02d988ce8960c9394d50719ed880d043d0013402
ocasusMaximus <za.jan@seznam.cz>
2020-03-17 19:56:35 +0100

    Functional Action with struts.xml

--- src/main/java/cz/uhk/fim/actions/TicketAction.java
+++ src/main/java/cz/uhk/fim/actions/TicketAction.java
@@ -1,7 +1,13 @@
 package cz.uhk.fim.actions;
 
 import com.opensymphony.xwork2.ActionSupport;
+import lombok.extern.log4j.Log4j2;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.springframework.stereotype.Component;
 
+@ParentPackage("struts-thymeleaf")
+@Component
+@Log4j2
 public class TicketAction extends ActionSupport {
 
     @Override
--- src/main/java/cz/uhk/fim/config/TicketStrutsFilter.java
+++ src/main/java/cz/uhk/fim/config/TicketStrutsFilter.java
@@ -7,9 +7,8 @@ import org.apache.struts2.dispatcher.servlet.StrutsServlet;
 import javax.servlet.annotation.WebFilter;
 
 
-@WebFilter
+@WebFilter("/*")
 public class TicketStrutsFilter extends StrutsPrepareAndExecuteFilter {
 
-    private static final long serialVersionUID = 4919365268043339351L;
 
 }
